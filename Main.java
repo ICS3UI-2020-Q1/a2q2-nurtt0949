d5dd950f4c84fb443a2be7dd0404fb8876cf3494
nurtt0949 <nurtt0949@wrdsb.ca>
2020-09-15 16:07:20 +0000

    Greater Number

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 /**
- *
+ *This program shows the greater number
  * @author 
  */
 public class Main {
@@ -9,7 +11,50 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //create a Scanner for user input
+   Scanner input = new Scanner(System.in);
+
+  //ask user to chose an integer
+  System.out.println("Enter an integer");
+
+  //get integer from user
+  int integer1 = input.nextInt();
+  
+   //ask user to chose another integer
+  System.out.println("Enter another integer");
+
+  //get integer from user
+  int integer2 = input.nextInt();
+
+  //calculate the greater number
+  if(integer1 > integer2){
+  
+  //tell the user the greater number
+  System.out.println("The greater number is " + integer1);
+
+ } else if (integer2 > integer1){ 
+
+  //tell user the greater number
+  System.out.println("The greater number is " + integer2);
+
+ }
+  //if user chose the same number
+  if(integer1 == integer2){
+
+  //tell them that both numbers are equall
+  System.out.println("There is no bigger number");
+  
+  
+  
+
+  
+
+
+   
+  
+
+  
+  
+  } 
   }
 }
